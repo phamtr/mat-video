8fa630878ddc1ec85566f8b75bfbcef339e0d712
Pham Van Truong <pvtruong115@gmail.com>
2019-06-18 10:15:51 +0700

    f

---
+++ src/app/sample.component.ts
@@ -0,0 +1,31 @@
+import { Component, VERSION } from '@angular/core';
+import * as buildInfo from './../../package.json';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css'],
+})
+export class SampleComponent {
+  version = VERSION.full;
+  appversion: string = (<any>buildInfo)['version'];
+
+  ngclass = 'mat-video-responsive';
+
+  src = 'assets/NASA.mp4';
+  title = 'NASA Rocket Launch';
+  width = 600;
+  height = 337.5;
+  autoplay = false;
+  preload = true;
+  loop = false;
+  quality = true;
+  download = true;
+  fullscreen = true;
+  keyboard = true;
+  color = 'primary';
+  spinner = 'spin';
+  poster = 'assets/NASA.jpg';
+  overlay = null;
+  muted = false;
+}
